229db1ec9853d9b007b6d6f422f1edff2e45d3f4
Marco Pereira <marcocsp@hotmail.com>
2021-05-03 00:02:18 +0100

    Added matcher/inbuilt-matchers/to-be-defined example

---
+++ src/app/matchers/inbuilt-matchers/to-be-defined.spec.ts
@@ -0,0 +1,17 @@
+describe('toBeDefined matcher', () => {
+  it('shoud validate any defined value', () => {
+    const num = 10;
+    expect(num).toBeDefined();
+
+    expect(NaN).toBeDefined();
+    expect(null).toBeDefined();
+  });
+
+  it('shoud not validate undefined', () => {
+    // const implicityUndefined;
+    const explicitlyUndefined = undefined;
+
+    // expect(implicityUndefined).not.toBeDefined();
+    expect(explicitlyUndefined).not.toBeDefined();
+  });
+});
