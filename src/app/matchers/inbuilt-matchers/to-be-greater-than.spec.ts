36475e85b5a7f0171d233b59192974c2184f9c51
Marco Pereira <marcocsp@hotmail.com>
2021-05-04 00:13:41 +0100

    Added matcher/inbuilt-matchers/to-be-greater-than example

---
+++ src/app/matchers/inbuilt-matchers/to-be-greater-than.spec.ts
@@ -0,0 +1,13 @@
+describe('toBeGreaterThan matcher', () => {
+  it('shoud validate a greater value as number', () => {
+    const casePi = 3.1415;
+
+    expect(casePi).toBeGreaterThan(3);
+  });
+
+  it('shoud validate a greater value as string', () => {
+    const casePi = '3.1415';
+
+    expect(casePi).toBeGreaterThan(3);
+  });
+});
