5ba132e4193b19214e5a7533d96aa1699a15a8ee
Marco Pereira <marcocsp@hotmail.com>
2021-05-04 00:12:37 +0100

    Added matcher/inbuilt-matchers/to-be-undefined example

---
+++ src/app/matchers/inbuilt-matchers/to-be-undefined.spec.ts
@@ -0,0 +1,14 @@
+describe('toBeUndefined matcher', () => {
+  it('shoud validate undefined values', () => {
+    const caseUndefined = undefined;
+    expect(caseUndefined).toBeUndefined();
+  });
+
+  it('shoud not validate false negatives', () => {
+    const caseFalse = false;
+    const caseNull = null;
+
+    expect(caseFalse).not.toBeUndefined();
+    expect(caseNull).not.toBeUndefined();
+  });
+});
