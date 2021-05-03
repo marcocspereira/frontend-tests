5704ea540967ae29d1a7eac6fe6c0d1de2441cad
Marco Pereira <marcocsp@hotmail.com>
2021-05-04 00:12:20 +0100

    Added matcher/inbuilt-matchers/to-contain example

---
+++ src/app/matchers/inbuilt-matchers/to-contain.spec.ts
@@ -0,0 +1,13 @@
+describe('toContain matcher', () => {
+  it('shoud validate with a string', () => {
+    const text = 'TaxIT Porto';
+    expect(text).toContain('Porto');
+    expect(text).not.toContain('Viseu');
+  });
+
+  it('shoud validate with an array', () => {
+    const array = ['lopes', 'da', 'silva'];
+    expect(array).toContain('silva');
+    expect(array).not.toContain('do');
+  });
+});
