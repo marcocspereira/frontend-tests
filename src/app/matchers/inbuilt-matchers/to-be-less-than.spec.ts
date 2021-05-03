ba5b3ce65d0ceaa02f946f398556411651750cfc
Marco Pereira <marcocsp@hotmail.com>
2021-05-04 00:13:29 +0100

    Added matcher/inbuilt-matchers/to-be-less-than example

---
+++ src/app/matchers/inbuilt-matchers/to-be-less-than.spec.ts
@@ -0,0 +1,13 @@
+describe('toBeLessThan matcher', () => {
+  it('shoud validate a smaller value as number', () => {
+    const casePi = 3.1415;
+
+    expect(casePi).toBeLessThan(4);
+  });
+
+  it('shoud validate a smaller value as string', () => {
+    const casePi = '3.1415';
+
+    expect(casePi).toBeLessThan(4);
+  });
+});
