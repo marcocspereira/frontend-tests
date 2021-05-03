508ed6fbec82e09cb3a28f334815727e597580ea
Marco Pereira <marcocsp@hotmail.com>
2021-05-04 00:13:01 +0100

    Added matcher/inbuilt-matchers/to-be-falsy example

---
+++ src/app/matchers/inbuilt-matchers/to-be-falsy.spec.ts
@@ -0,0 +1,24 @@
+describe('toBeFalsy matcher', () => {
+  it('shoud validate falsy values', () => {
+    const caseUndefined = undefined;
+    const caseNaN = NaN;
+    const caseZero = 0;
+    const caseEmpty = '';
+    const caseNull = null;
+
+    expect(caseUndefined).toBeFalsy();
+    expect(caseNaN).toBeFalsy();
+    expect(caseZero).toBeFalsy();
+    expect(caseEmpty).toBeFalsy();
+    expect(caseNull).toBeFalsy();
+  });
+
+  it('shoud not validate not falsy values', () => {
+    const caseNumber = 1;
+    const caseText = 'TaxIT';
+    const caseTrue = true;
+    expect(caseNumber).not.toBeFalsy();
+    expect(caseText).not.toBeFalsy();
+    expect(caseTrue).not.toBeFalsy();
+  });
+});
