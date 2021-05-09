5124b20ba5ad6b68aa952d777e01bb7cf93fafca
Marco Pereira <marcocsp@hotmail.com>
2021-05-09 19:56:37 +0100

    Added HttpClientModule and FormsModule dependencies to AooNidule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ZeComponent } from './components/ze/ze.component';
+import { CoinGeckoComponent } from './components/coin-gecko/coin-gecko.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ZeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, ZeComponent, CoinGeckoComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
