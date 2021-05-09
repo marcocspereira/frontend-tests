0581fe0a8ab546085965aad64c0e98ebb0140ab3
Marco Pereira <marcocsp@hotmail.com>
2021-05-09 12:26:30 +0100

    Created a spec for CoinGeckoService using mockProvider

---
+++ src/app/services/coin-gecko/coin-gecko-mock-provider.service.spec.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import {
+  createServiceFactory,
+  mockProvider,
+  SpectatorService,
+} from '@ngneat/spectator';
+import { CoinGeckoService } from './coin-gecko.service';
+import { dogecoin } from './mocks/dogecoin.mock';
+
+describe('CoinGeckoService', () => {
+  let spectator: SpectatorService<CoinGeckoService>;
+  const createService = createServiceFactory({
+    service: CoinGeckoService,
+    providers: [
+      mockProvider(HttpClient, {
+        get: () => 'I am a mocked get',
+      }),
+    ],
+    mocks: [],
+  });
+
+  beforeEach(() => (spectator = createService()));
+
+  describe('getCoin', () => {
+    it('should call CoinGecko API through HttpClient', () => {
+      /** arrange */
+      let result: any;
+      const url: string = 'https://api.coingecko.com/api/v3/coins/dogecoin';
+      const apiSpy = spyOn(spectator.inject(HttpClient), 'get');
+      apiSpy.withArgs(url).and.returnValue(dogecoin);
+
+      /** act */
+      result = spectator.service.getCoin('dogecoin');
+
+      /** assert */
+      expect(apiSpy).toHaveBeenCalled();
+      expect(result).toEqual(dogecoin);
+    });
+  });
+
+  describe('getTrends', () => {
+    it('should return a list of coins', () => {
+      /** arrange */
+      /** act */
+      /** assert */
+    });
+  });
+});
