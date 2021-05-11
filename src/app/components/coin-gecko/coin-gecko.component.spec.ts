542990c5ad43252a81802c406501d5909a92138a
Marco Pereira <marcocsp@hotmail.com>
2021-05-11 03:49:02 +0100

    Removed getAllCoins

--- src/app/components/coin-gecko/coin-gecko.component.spec.ts
+++ src/app/components/coin-gecko/coin-gecko.component.spec.ts
@@ -1,8 +1,10 @@
-import { Spectator, createComponentFactory } from '@ngneat/spectator';
+import { createComponentFactory } from '@ngneat/spectator';
 
 import { CoinGeckoComponent } from './coin-gecko.component';
 
 xdescribe('CoinGeckoComponent', () => {
-  let spectator: Spectator<CoinGeckoComponent>;
-  const createComponent = createComponentFactory(CoinGeckoComponent);
+  const createComponent = createComponentFactory({
+    component: CoinGeckoComponent,
+    providers: [],
+  });
 });
--- src/app/components/coin-gecko/coin-gecko.component.ts
+++ src/app/components/coin-gecko/coin-gecko.component.ts
@@ -1,6 +1,7 @@
 import { Component, AfterViewInit, Input } from '@angular/core';
 import { CoinGeckoService } from 'src/app/services/coin-gecko/coin-gecko.service';
 import { Coin } from 'src/app/services/coin-gecko/coin.type';
+import { DataService } from 'src/app/services/data/data.service';
 
 @Component({
   selector: 'coin-gecko',
@@ -12,23 +13,26 @@ export class CoinGeckoComponent implements AfterViewInit {
   coin: Coin | undefined;
   topCoins: Array<Coin> | undefined;
 
-  constructor(private _coinGeckoService: CoinGeckoService) {}
+  constructor(
+    private _coinGeckoService: CoinGeckoService,
+    private _dataService: DataService
+  ) {}
 
   ngAfterViewInit(): void {
-    this.getAllCoins();
+    this._dataService.reset();
   }
 
+  /**
+   * Method that gets data related to a given coin ID from CoinGecko API
+   * through CoinGeckoService. Also persists in session storage with DataService.
+   * @param selectedCoin input data from template
+   */
   getCoinData(selectedCoin: any) {
     const coinId = selectedCoin.coinId;
     this.coinId = coinId;
-    this._coinGeckoService
-      .getCoin(coinId)
-      .subscribe((coinData) => (this.coin = coinData));
-  }
-
-  getAllCoins() {
-    this._coinGeckoService.getCoins().subscribe((coins) => {
-      this.topCoins = coins;
+    this._coinGeckoService.getCoin(coinId).subscribe((coinData) => {
+      this.coin = coinData;
+      this._dataService.persistItem(selectedCoin, coinData);
     });
   }
 }
